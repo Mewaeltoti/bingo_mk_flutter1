119e277ac122b5875d6056f83e4a5a35889d3443
Mewaeltoti <mewaeltoti@gmail.com>
2026-05-19 09:46:24 +0300

    feat: initialize Android build configuration with Gradle 8.9 and project settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
-
+compileSdk = 35
 android {
     namespace = "com.bingomk.bingo_mk"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -25,8 +25,8 @@ android {
         applicationId = "com.bingomk.bingo_mk"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
