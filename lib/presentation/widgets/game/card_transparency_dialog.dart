fe12e3cac8122c13df776723487bb75867d73385
Mewaeltoti <mewaeltoti@gmail.com>
2026-05-29 11:02:26 +0300

    x1223

--- lib/presentation/widgets/game/card_transparency_dialog.dart
+++ lib/presentation/widgets/game/card_transparency_dialog.dart
@@ -13,7 +13,7 @@ void showCardTransparencyDialog(
   Map<String, dynamic>? found;
   List<int> numbersList = [];
   String cardNo = item;
-  String phone = "ስልክ ቁጥር: 0910117997";
+  String phone = "Phone";
 
   if (isBlocked) {
     final userCard = state.userCards.firstWhere(
@@ -28,7 +28,7 @@ void showCardTransparencyDialog(
     if (userCard.id.isNotEmpty) {
       numbersList = userCard.numbers.expand((row) => row).toList();
       cardNo = userCard.cardNo.toString();
-      phone = "የእርስዎ የታገደ ካርቴላ";
+      phone = "Your Blocked Card";
     }
   } else {
     final searchList = isWinner ? state.rawWinnersData : state.rawClaimsData;
@@ -242,7 +242,7 @@ void showCardTransparencyDialog(
                         padding: const EdgeInsets.symmetric(vertical: 12),
                       ),
                       child: const Text(
-                        "ዕድሉ",
+                        "Back",
                         style: TextStyle(
                           color: Colors.white,
                           fontWeight: FontWeight.w900,
