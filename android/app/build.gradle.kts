bef0e4b057f3070be40e884ae802530570db164e
Mewaeltoti <mewaeltoti@gmail.com>
2026-06-03 18:24:16 +0300

    gvvew

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
